4e7ee46a156ac04301d3920c3f7d0cc80dc67111
Felipe Spinelli <felipespinelli19@gmail.com>
2023-04-30 01:35:29 -0300

    :fire: Removes UI proj

--- src/ArquiveSe.UI/Clients/ArquiveSeApi/Models/Requests/CreateAccountRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ArquiveSe.UI.Clients.ArquiveSeApi.Models.Requests
-{
-    public record CreateAccountRequest
-    {
-        public string UserId { get; init; } = null!;
-    }
-}
--- src/ArquiveSe.UI/Clients/ArquiveSeApi/Models/Response/CreateAccountResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ArquiveSe.UI.Clients.ArquiveSeApi.Models.Response
-{
-    public class CreateAccountResponse
-    {
-        public string AccountId { get; set; } = null!;
-    }
-}
